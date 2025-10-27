54c2a257fdaf937801fc38ee24389d6485f70c4b
JazilAlamMalik <pcjazil14737@gmail.com>
2025-10-27 23:04:22 +0500

    first comment

---
+++ scd lab/hello.py
@@ -0,0 +1,16 @@
+import random
+
+print("Welcome to the Number Guessing Game!")
+number = random.randint(1, 10)
+attempts = 0
+
+while True:
+    guess = int(input("Guess a number between 1 and 10: "))
+    attempts += 1
+    if guess == number:
+        print(f"🎉 Correct! You guessed it in {attempts} tries.")
+        break
+    elif guess < number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
